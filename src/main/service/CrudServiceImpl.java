e19f5fbe37c5be6c015b626fa607eda4766875d6
Ricardo Quezada <qf7.ricardo@gmail.com>
2017-10-30 22:27:30 -0600

    Modificaciones menores a clase de servicio

--- src/main/service/CrudServiceImpl.java
+++ src/main/service/CrudServiceImpl.java
@@ -1,15 +1,7 @@
 /* Reemplazo de Crud.java */
 package main.service;
 
-import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -19,14 +11,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.CacheControl;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.EntityTag;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 
+import main.api.CrudService;
 import main.dao.DeportesDao;
 import main.dao.DeportesDaoI;
 import main.dao.EstadosDao;
@@ -50,41 +38,10 @@ import main.modelo.excepciones.DeportesVaciosException;
 import main.modelo.excepciones.NombreIncorrectoException;
 import main.utilidades.Par;
 
-import main.api.CrudService;
-
-/**
- * Servlet de ejercicio de Crud.
- *
- * <p>
- * Clase controlador que liga las operaciones del usuario en las vistas (*.jsp y *.html) con las
- * clases internas del modelo.
- *
- * <p>
- * Operaciones disponibles:
- * <ol>
- * <li>GET /crud </li>
- * <li>POST /crud/obtenerMunicipios </li>
- * <li>POST /crud/colocarPersona </li>
- * <li>POST /crud/actualizarPersona </li>
- * <li>POST /crud/borrarPersona </li>
- * <li>POST /crud/obtenerNumeroPersonas </li>
- * <li>POST /crud/obtenerPersonas </li>
- * <li>POST /crud/obtenerPersona </li>
- * </ol>
- *
- * <p>
- * Cada una de estas operaciones se abstrae en un método privado distinto de esta clase. Para más
- * información sobre los propios métodos, ver los javadocs de cada método.
- */
 @Path("/")
 @Consumes(MediaType.APPLICATION_JSON)
-
 public class CrudServiceImpl implements CrudService {
 
-  @Context
-  private UriInfo uri;
-
-  /* DAOs */
   private final EstadosDaoI estadosDao = new EstadosDao();
   private final DeportesDaoI deportesDao = new DeportesDao();
   private final MunicipiosDaoI municipiosDao = new MunicipiosDao();
@@ -92,12 +49,13 @@ public class CrudServiceImpl implements CrudService {
   private final SexosDaoI sexosDao = new SexosDao();
 
   @Override
-  public void finalize() {
+  public void finalize() throws Throwable {
     estadosDao.cerrarConexion();
     deportesDao.cerrarConexion();
     municipiosDao.cerrarConexion();
     personasDao.cerrarConexion();
     sexosDao.cerrarConexion();
+    super.finalize();
   }
 
   @Override
@@ -105,8 +63,8 @@ public class CrudServiceImpl implements CrudService {
   @Path("/obtenerEstados")
   @Produces(MediaType.APPLICATION_JSON)
   public Response obtenerEstados() {
-
-    Response.ResponseBuilder responseBuilder = null;
+    
+    Response.ResponseBuilder responseBuilder;
     ArrayList<Estado> catalogoEstados = estadosDao.obtenerEstados();
     responseBuilder = Response.ok(catalogoEstados, "application/json;charset=UTF-8");
     return responseBuilder.build();
@@ -118,8 +76,8 @@ public class CrudServiceImpl implements CrudService {
   @Path("/obtenerSexos")
   @Produces(MediaType.APPLICATION_JSON)
   public Response obtenerSexos() {
-
-    Response.ResponseBuilder responseBuilder = null;
+    
+    Response.ResponseBuilder responseBuilder;
     ArrayList<Sexo> catalogoSexos = sexosDao.obtenerSexos();
     responseBuilder = Response.ok(catalogoSexos, "application/json;charset=UTF-8");
     return responseBuilder.build();
@@ -131,8 +89,8 @@ public class CrudServiceImpl implements CrudService {
   @Path("/obtenerDeportes")
   @Produces(MediaType.APPLICATION_JSON)
   public Response obtenerDeportes() {
-
-    Response.ResponseBuilder responseBuilder = null;
+    
+    Response.ResponseBuilder responseBuilder;
     ArrayList<Deporte> catalogoDeportes = deportesDao.obtenerDeportes();
     responseBuilder = Response.ok(catalogoDeportes, "application/json;charset=UTF-8");
     return responseBuilder.build();
@@ -144,8 +102,8 @@ public class CrudServiceImpl implements CrudService {
   @Path("/obtenerMunicipios/{id}")
   @Produces(MediaType.APPLICATION_JSON)
   public Response obtenerMunicipios(@PathParam("id") int idEstado) {
-
-    Response.ResponseBuilder responseBuilder = null;
+    	
+    Response.ResponseBuilder responseBuilder;
     Estado estado = new Estado();
     estado.setIdEstado(idEstado);
     ArrayList<Municipio> catalogoMunicipios = municipiosDao.obtenerMunicipios(estado);
@@ -162,7 +120,7 @@ public class CrudServiceImpl implements CrudService {
       @PathParam("max") int maximo, @QueryParam("query") String consulta) {
 
     consulta = (consulta == null) ? "" : consulta;
-    Response.ResponseBuilder responseBuilder = null;
+    Response.ResponseBuilder responseBuilder;
     Par<Integer, ArrayList<Persona>> personas
         = personasDao.obtenerPersonas(minimo, maximo, consulta);
     responseBuilder = Response.ok(personas, "application/json;charset=UTF-8");
@@ -176,7 +134,7 @@ public class CrudServiceImpl implements CrudService {
   @Produces(MediaType.APPLICATION_JSON)
   public Response obtenerPersona(@PathParam("id") int idUsuario) {
 
-    Response.ResponseBuilder responseBuilder = null;
+    Response.ResponseBuilder responseBuilder;
     Persona persona = personasDao.obtenerPersona(idUsuario);
     responseBuilder = Response.ok(persona, "application/json;charset=UTF-8");
     return responseBuilder.build();
@@ -190,7 +148,7 @@ public class CrudServiceImpl implements CrudService {
   @Consumes(MediaType.APPLICATION_JSON)
   public Response colocarPersona(Persona persona) {
     System.out.println("DEBUG: " + persona);
-    Response.ResponseBuilder responseBuilder = null;
+    Response.ResponseBuilder responseBuilder;
     int codigo;
     try {
       Validaciones.validarPersona(persona);
@@ -245,8 +203,8 @@ public class CrudServiceImpl implements CrudService {
   @Path("/borrarPersona/{id}")
   @Produces(MediaType.TEXT_PLAIN)
   public Response borrarPersona(@PathParam("id") int idUsuario) {
-
-    Response.ResponseBuilder responseBuilder = null;
+    
+    Response.ResponseBuilder responseBuilder;
     Persona persona = new Persona();
     persona.setIdPersona(idUsuario);
     personasDao.borrarPersona(persona);
